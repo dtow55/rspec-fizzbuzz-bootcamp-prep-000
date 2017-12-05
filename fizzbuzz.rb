241e6ca5f794a5637e429559ede77be148c153ef
David J Towster <djt4xe@virginia.edu>
2017-12-05 17:17:05 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(number)
+  if (number / 3).is_a? Integer && (number / 3).is_a? Integer
+    "Fizzbuzz"
+  elsif (number / 3).is_a? Integer
+    "Fizz"
+  else (number / 5).is_a? Integer
+    "Buzz"
+  end
+end
